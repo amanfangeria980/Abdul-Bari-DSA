a6a11c65cbd0443d2d6739eaa1352832428c0313
amanfangeria980 <amanfangeria980@gmail.com>
2022-05-22 23:46:50 +0530

    arraypractice.cpp updated

--- arraypractice.cpp
+++ arraypractice.cpp
@@ -31,8 +31,7 @@ int main(){
     // cout<<arr[2];
     // cout<<arr[3];
     // cout<<arr[4];
-
-    //it will store nos. consecutively.
+    //all elements have garbage values
 
     // int arr2[5]={};
     // cout<<arr2[0];
@@ -40,8 +39,8 @@ int main(){
     // cout<<arr2[2];
     // cout<<arr2[3];
     // cout<<arr2[4];
+    //all elements have zero value
 
-    // same as above
 
     // int arr[]={1,3};
     // cout<<arr[0]<<endl;
@@ -50,6 +49,7 @@ int main(){
     // cout<<arr[4]<<endl;
     // cout<<arr[5]<<endl;
     // cout<<arr[6]<<endl;
+    //left up values have garbage values following from index 2
     // cout<<sizeof(arr);
 
     //garbage values for rest of them which are not declared
@@ -74,8 +74,8 @@ int main(){
     // but if it runs, the syntax would be like this:
     // int arr[n]={1,2,3,4,5};
     // cout<<sizeof(arr)<<endl;
-    // cout<<sizeof(arr[0])<<endl; //garbage values
-    // cout<<sizeof(arr[1])<<endl; //garbage values
+    // cout<<sizeof(arr[0])<<endl; 
+    // cout<<sizeof(arr[1])<<endl;
 
     //all the values will be garbage values for now and can be assigned now like this:
  
